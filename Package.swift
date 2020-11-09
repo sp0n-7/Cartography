f772da0de51ce44658f3ed149308f790929dd25f
Max Mamis <max@prolificinteractive.com>
2020-11-09 17:23:06 -0500

    Revert "add dependencies"
    This reverts commit fa93c20fe7c6bf56243528f7b702779a6980aa02.

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "Cartography", targets: ["Cartography"])
     ],
     targets: [
-        .target(name: "Cartography", dependencies: ["UIKit", "Foundation"], path: "Cartography")
+        .target(name: "Cartography", path: "Cartography")
     ]
 )
